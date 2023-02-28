8ce6d5a01514423f251970ec5b3ed823ee7c0a12
Joshua Kelly <jmk3378@rit.edu>
2023-02-28 17:25:52 -0500

    Created maintenance script.

---
+++ maintenance.py
@@ -0,0 +1,11 @@
+'''
+TODO
+This is where the script will be which should check on the following:
+    SE Linux is enabled.
+
+    Make sure the host is updating.
+
+    Autoremove old packages
+
+    Ports: 80, 443, 22, 3000, 3001
+'''
