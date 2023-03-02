c4dd4c7bf7cd358b45d1f5a7679385458b1b1aa7
Joshua Kelly <jmk3378@rit.edu>
2023-03-02 14:46:46 -0500

    Organized functions.

--- maintenance.py
+++ maintenance.py
@@ -13,25 +13,28 @@ This is where the script will be which should check on the following:
 import os
 
 
-
 def check_sestatus():
     sestatus = "sudo sestatus"
+    sestatus_result = os.system(sestatus)
+
+    print("SE Status Result: " + sestatus_result)
 
 
 def check_ports():
-    open_ports = [80, 443, 22, 3000, 3001]
+    open_ports = [80, 443, 22, 3000, 3001, 5000]
 
 
 def check_update():
     update = "sudo apt-get -y update"
     upgrade = "sudo apt-get -y upgrade"
     autoremove = "sudo apt-get -y autoremove"
+    update_result = os.system(update)
+    upgrade_result = os.system(upgrade)
+    autoremove_result = os.system(autoremove)
 
-    
+    print("Upgrade Result: " + update_result + "\n" + "Upgrade Result: " +
+          upgrade_result + "\n" + "Autoremove Result: " + autoremove_result)
 
-
-check_sestatus()
-check_ports()
-check_update()
-
-
+# check_sestatus()
+# check_ports()
+# check_update()
