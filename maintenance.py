4a079ecaa3e723666025dab9106f4d3bfc8e23be
Joshua Kelly <jmk3378@rit.edu>
2023-03-02 14:10:54 -0500

    Progress So Far

--- maintenance.py
+++ maintenance.py
@@ -9,3 +9,28 @@ This is where the script will be which should check on the following:
 
     Ports: 80, 443, 22, 3000, 3001
 '''
+import os
+
+
+
+def check_sestatus():
+    sestatus = "sudo sestatus"
+
+
+def check_ports():
+    open_ports = [80, 443, 22, 3000, 3001]
+
+
+def check_update():
+    update = "sudo apt-get -y update"
+    upgrade = "sudo apt-get -y upgrade"
+    autoremove = "sudo apt-get -y autoremove"
+
+    
+
+
+check_sestatus()
+check_ports()
+check_update()
+
+
